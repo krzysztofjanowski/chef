af0dd97df34b5011bbdc2096c4675beaf027a950
krzysztofjanowski <krzysztof.janowski@newvoicemedia.com>
2018-12-05 23:18:20 +0000

    initial commit

---
+++ workstation/recipes/setup.rb
@@ -0,0 +1,25 @@
+
+#
+# The package, named 'tree', is installed
+#
+# @see https://docs.chef.io/resource_package.html
+#
+package 'tree'
+
+package 'vim' do
+  action :install
+end
+#
+# The template, named '/etc/motd' is created
+#   with the source 'motd.erb'
+#
+# @see https://docs.chef.io/resource_template.html
+#
+# The template file, named 'motd.erb' can be found in the
+#   the templates folder under default. This means for all platforms:
+#
+# @see https://docs.chef.io/resource_template.html#file-specificity
+#
+template '/etc/motd' do
+  source 'motd.erb'
+end
