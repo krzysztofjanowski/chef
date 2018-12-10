18333db4a35b48bdfab9445b49ad3e9909cf7368
krzysztofjanowski <krzysztof.janowski@newvoicemedia.com>
2018-12-10 22:06:03 +0000

    added myhaproxy

---
+++ udemy chef-repo/cookbooks/my_haproxy/README.md
@@ -0,0 +1,4 @@
+# my_haproxy
+
+TODO: Enter the cookbook description here.
+
---
+++ udemy chef-repo/cookbooks/my_haproxy/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: my_haproxy
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
